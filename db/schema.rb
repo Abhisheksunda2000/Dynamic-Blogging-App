d4dec389667e52adddaa01983d501332e05912f9
Abhishek <ab9672045603@gmail.com>
2023-08-10 22:32:40 +0530

    password_digest is added to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_144503) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_165802) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "topic"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_144503) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
