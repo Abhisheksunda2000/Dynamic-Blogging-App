900d2abcb6f461eb7fdc2222e4555f3e2c58c52e
Abhishek <ab9672045603@gmail.com>
2023-08-10 20:33:36 +0530

    add one_to_many_association_between_users_and_posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_130423) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_144503) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "topic"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_130423) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts_tables", force: :cascade do |t|
