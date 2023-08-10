55f5f225380e9d44ba879f8527524ee790b45b3a
Abhishek <ab9672045603@gmail.com>
2023-08-10 18:48:59 +0530

    create a user model and users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_072105) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_130423) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "topic"
@@ -25,6 +25,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_072105) do
     t.string "topic"
     t.text "text"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
