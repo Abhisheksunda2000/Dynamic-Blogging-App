c4700710f2ceaa8db3794442bf382068183b86d5
Abhishek <ab9672045603@gmail.com>
2023-08-11 19:50:35 +0530

    login and logout working

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   resources :posts, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   get 'signup', to: "users#new"
   resources :users, except: [:new]
+
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+
 end
