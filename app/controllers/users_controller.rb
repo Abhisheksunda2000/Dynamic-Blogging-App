4fc89884ebe838026bdfc3fd5034d4034581f840
Abhishek <ab9672045603@gmail.com>
2023-08-12 22:57:40 +0530

    Authentication helper method implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-           def show
-             @user = User.find(params[:id])
+            before_action :set_user, only: [:show, :edit, :update]
+            def show  
              @posts = @user.posts.paginate(:page => params[:page], :per_page => 1)
            end
 
@@ -13,11 +13,9 @@ class UsersController < ApplicationController
            end
 
            def edit
-            @user = User.find(params[:id])
            end
 
            def update
-            @user = User.find(params[:id])
             if(@user.update(user_params))
                 flash[:notice] = "You have successfully updated the signed in info"
                 redirect_to user_path
@@ -29,6 +27,7 @@ class UsersController < ApplicationController
            def create
             @user = User.new(user_params)
             if(@user.save)
+                session[:user_id] = @user.id
                 flash[:notice] = "Welcome to the SocialApp #{@user.username}, you have successfully signed up"
                 redirect_to @user
             else
@@ -40,5 +39,9 @@ class UsersController < ApplicationController
 
            def user_params
                params.require(:user).permit(:username, :email, :password)
+           end
+
+           def set_user
+               @user = User.find(params[:id])
            end
 end
