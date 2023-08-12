f40b551571763f2b2c5f5130f8d44ee12121eeb1
Abhishek <ab9672045603@gmail.com>
2023-08-12 18:30:56 +0530

    login and logout feature added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
-    validates :title, presence:true, length: {minimum:5, maximum:15}
-    validates :topic, presence:true, length: {minimum:5, maximum:25}
-    validates :text,  presence:true, length: {minimum:5, maximum:100}
+    validates :title, presence:true, length: {minimum:5, maximum:100}
+    validates :topic, presence:true, length: {minimum:5, maximum:100}
+    validates :text,  presence:true, length: {minimum:5, maximum:1000}
 end
